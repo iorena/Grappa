9c0ea6be30fc53b6e275e1f5bb7228e83fd2c9e4
iorena <essi.salmenkivi@helsinki.fi>
2016-03-15 12:14:57 +0200

    env var port

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 var http = require('http'),
     pg = require('pg');
 
-var port = 6667;
+var port = process.env.port || 6667;
     
 module.exports = (function() {
 
-        http.createServer(function(req, res) {
-            res.writeHead(200, {'Content-Type' : 'text/plain'});
-            res.end('moi');
-        }).listen(port);
+    http.createServer(function(req, res) {
+        res.writeHead(200, {'Content-Type' : 'text/plain'});
+        res.end('moi');
+    }).listen(port);
 
 
-    }());
+}());
