00f6b08f66edf331275a50eedeea77b70d833b11
iorena <essi.salmenkivi@helsinki.fi>
2016-03-15 14:45:36 +0200

    test url

--- test/app_test.js
+++ test/app_test.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 describe('app', function() {
     it('should load', function() {
-        var url = 'http://localhost:' + process.env.PORT; 
+        var url = 'http://tktl-grappa.herokuapp.com:' + process.env.PORT; 
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
         });
