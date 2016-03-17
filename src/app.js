ac56be029b0e72ea460d8761fb32dab57c6a1111
iorena <essi.salmenkivi@helsinki.fi>
2016-03-17 11:22:32 +0200

    added express back, configuring REST routes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,33 @@
-var http = require('http'),
-    pg = require('pg');
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    pg = require("pg"),
+    app = express();
 
 var port = process.env.PORT || 6667,
     dburl = process.env.DATABASE_URL;
     
 module.exports = (function() {
 
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    var router = express.Router();
+
     var dbclient = new pg.Client(dburl);
     dbclient.connect();
     dbclient.end();
-    
-    http.createServer(function(req, res) {
-        res.writeHead(200, {'Content-Type' : 'text/plain'});
-        res.end('porkkana');
-    }).listen(port);
 
+    /* set the routes */
+
+    router.get("/", function(request, result) {
+        result.json({ message : "This is the default page. Nothing to see here" });
+    });
+
+    router.get("/theses", function(request, result) {
+        result.json({ message : "This is where I list all the theses \o/" });
+    });
+
+    app.use("/", router);
+    
+    app.listen(port);
 
 }());
