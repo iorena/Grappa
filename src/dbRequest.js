02cf5645645322a6f847429c930c7db218164574
iorena <essi.salmenkivi@helsinki.fi>
2016-03-21 14:09:09 +0200

    GET and POST routes access db

--- src/dbRequest.js
+++ src/dbRequest.js
@@ -1,18 +1,32 @@
 var dbUrl = process.env.DATABASE_URL;
+var tables = require("../sql/tables.js");
 var Sequelize = require("sequelize");
 
-var seq = new Sequelize(dbUrl);
-
-/* define models here */
+var seq = new Sequelize(dbUrl, { dialectOptions: { ssl: true } });
 
 module.exports = {
 
     add : function(params, table) {
-        //sequelize.create(params);
+        tables[table].create(params);
     },
 
-    list : function(params, table) {
-
+    list : function(params, table, callback) {
+        tables[table].findAll({where:params}).then(function(results) {
+            console.log(results);
+            callback(results.map(x => JSON.stringify(x.dataValues)));
+        });
+    },
+    /* this doesn't work yet */ 
+    dump : function(callback) {
+        var res = [];
+        for (var model in tables)
+        {
+            tables[model].findAll().then(function(results)
+            {
+                res.push(results.dataValues);
+                console.log(res);
+            });
+        }
+        callback(res);
     }
-
 };
