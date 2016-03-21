eb9c3982e62ec424e3afdc7677d3b028c7e570c0
iorena <essi.salmenkivi@helsinki.fi>
2016-03-21 16:25:12 +0200

    add doc strings

--- src/dbRequest.js
+++ src/dbRequest.js
@@ -4,18 +4,42 @@ var Sequelize = require("sequelize");
 
 var seq = new Sequelize(dbUrl, { dialectOptions: { ssl: true } });
 
+function stringify(obj) {
+    return JSON.stringify(obj, function(key, value) {
+        return value;
+    }, 2); 
+}
+
 module.exports = {
+
+    /*
+     * Makes an INSERT query to the specified @table
+     *
+     * @params object containing values to add
+     * @table sql table to add into, mapping in tables.js
+     *
+     * doesn't @return anything atm, should add
+     */
 
     add : function(params, table) {
         tables[table].create(params);
     },
 
+    /*
+     * Makes a SELECT query to the specified @table
+     *
+     * @params filter parameters
+     * @table sql table (name of model)
+     *
+     * @return list of db objects
+     */
+
     list : function(params, table, callback) {
-        tables[table].findAll({where:params}).then(function(results) {
-            console.log(results);
-            callback(results.map(x => JSON.stringify(x.dataValues)));
+        tables[table].findAll({where:params, raw: true}).then(function(results) {
+            callback(results.map(x => x.dataValues));
         });
     },
+
     /* this doesn't work yet */ 
     dump : function(callback) {
         var res = [];
@@ -23,8 +47,7 @@ module.exports = {
         {
             tables[model].findAll().then(function(results)
             {
-                res.push(results.dataValues);
-                console.log(res);
+                res.push(results.map(x => stringify(x.dataValues)));
             });
         }
         callback(res);
