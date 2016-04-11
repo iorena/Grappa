6669ecdf56780326ade67ec6ad8bc5821ed904e7
iorena <essi.salmenkivi@helsinki.fi>
2016-04-11 11:32:46 +0300

    fix lint

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ const expect = require("chai").expect;
 describe("app", () => {
   let url = "http://localhost:9876";
   it("should load", () => {
-    request(url, function(error, response, body) {
+    request(url, (error, response) => {
       expect(response.statusCode).to.equal(200);
     });
   });
@@ -14,7 +14,7 @@ describe("app", () => {
   describe("thesis route", () => {
     url += "/theses";
     it("accepts request parameters", () => {
-      request(url + "?testparam=666", function(error, response, body) {
+      request(url + "?testparam=666", (error, response) => {
         expect(response.body.result.testparam).to.equal(666);
       });
     });
@@ -23,7 +23,7 @@ describe("app", () => {
   describe("councilmeeting route", () => {
     url += "/councilmeetings";
     it("accepts request parameters", () => {
-      request(url + "?testparam=666", function(error, response, body) {
+      request(url + "?testparam=666", (error, response) => {
         expect(response.body.result.testparam).to.equal(666);
       });
     });
