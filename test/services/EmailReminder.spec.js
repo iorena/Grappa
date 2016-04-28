7e25baacc44127ac4dc08535210b749327033cf2
Henri Piironen <hcpiiron@sf-10.cs.helsinki.fi>
2016-04-28 14:44:59 +0300

    Fix tests

--- test/services/EmailReminder.spec.js
+++ test/services/EmailReminder.spec.js
@@ -1,72 +1,98 @@
-"use strict";
+      "use strict";
 
-const request = require("request");
-const expect = require("chai").expect;
-const Thesis = require("../../models/thesis");
-const Sender = require("../../services/EmailSender");
-const Reminder = require("../../services/EmailReminder");
-const Sinon = require("sinon");
-const User = require("../../models/user");
-const EmailStatus = require("../../models/email_status");
+      const request = require("request");
+      const expect = require("chai").expect;
+      const Thesis = require("../../models/thesis");
+      const Sender = require("../../services/EmailSender");
+      const Reminder = require("../../services/EmailReminder");
+      const Sinon = require("sinon");
+      const User = require("../../models/user");
+      const EmailStatus = require("../../models/email_status");
 
-const thesis = require("../mockdata/database").thesis;
+      const thesis = require("../mockdata/database").thesis;
 
-let calledParams = {};
-Sinon.stub(Sender, "sendEmail", (to, subject, body) => {
-  calledParams = {
-    to,
-    subject,
-    body,
-  };
-  return Promise.resolve(calledParams);
-});
-Sinon.stub(EmailStatus, "saveOne", function (params) {
-  return Promise.resolve(params);
-});
+      let calledParams = {};
 
-describe("EmailReminder", () => {
-  describe("sendStudentReminder(thesis)", () => {
-    it("should call sendEmail with correct values", (done) => {
-      const email = "pertti@perttinen.fi";
-      Reminder.sendStudentReminder(email, "ASDF123")
-      .then(status => {
-        expect(calledParams.to).to.equal(email);
-        expect(calledParams.subject).to.equal("REMINDER: Submit your thesis to eThesis");
-        done();
+
+      describe("EmailReminder", () => {
+      before(() => {
+          Sinon.stub(Sender, "sendEmail", (to, subject, body) => {
+        calledParams = {
+          to,
+          subject,
+          body,
+        };
+              return Promise.resolve(calledParams);
       });
+      Sinon.stub(EmailStatus, "saveOne", function (params) {
+        return Promise.resolve(params);
+      });
+      
+      Sinon.stub(Thesis, "findOne", () => {
+            return Promise.resolve();
+          });
     });
-  });
-  describe("sendPrintPersonReminder(thesis)", () => {
-    it("should call sendEmail with correct values", (done) => {
-      Sinon.stub(User, "findOne", (params) => {
-        if (typeof params.role !== "undefined" && params.role === "print-person") {
-            return Promise.resolve({
-              id: 2,
-              name: "B Virtanen",
-              title: "print-person",
-              email: "printperson@gmail.com",
-              admin: true,
+        after(() => {
+         
+        });
+
+
+        describe("sendStudentReminder(thesis)", () => {
+          it("should call sendEmail with correct values", (done) => {
+            const email = "pertti@perttinen.fi";
+            Reminder.sendStudentReminder(email, "ASDF123")
+            .then(status => {
+              expect(calledParams.to).to.equal(email);
+              expect(calledParams.subject).to.equal("REMINDER: Submit your thesis to eThesis");
+              done();
             });
-          } else {
-            return Promise.resolve(null);
-          }
+          });
+        });
+        describe("sendPrintPersonReminder(thesis)", () => {
+          it("should call sendEmail with correct values", (done) => {
+            Sinon.stub(User, "findOne", (params) => {
+              if (typeof params.role !== "undefined" && params.role === "print-person") {
+                  return Promise.resolve({
+                    id: 2,
+                    name: "B Virtanen",
+                    title: "print-person",
+                    email: "printperson@gmail.com",
+                    admin: true,
+                  });
+                } else {
+                  return Promise.resolve(null);
+                  }
+            });
+            Reminder.sendPrintPersonReminder(thesis)
+            .then(status => {
+              expect(calledParams.to).to.equal("printperson@gmail.com");
+              expect(calledParams.subject).to.equal("NOTE: Upcoming councilmeeting");
+              User.findOne.restore();
+              done();
+            });
+          });
+        });
+        describe("sendProfessorReminder(thesis)", () => {
+          it("should call sendEmail with correct values", (done) => {
+            Sinon.stub(User, "findOne", (params) => {
+              if (typeof params.role !== "undefined" && params.role === "professor") {
+                  return Promise.resolve({
+                    id: 2,
+                    name: "B Virtanen",
+                    title: "professor",
+                    email: "professor@gmail.com",
+                    admin: true,
+                  });
+                } else {
+                  return Promise.resolve(null);
+                  }
+            });
+            Reminder.sendProfessorReminder(thesis)
+            .then(status => {
+              expect(calledParams.to).to.equal("professor@gmail.com");
+              expect(calledParams.subject).to.equal("REMINDER: Submit your evaluation");
+              done();
+            });
+          });
+        });
       });
-      Reminder.sendPrintPersonReminder(thesis)
-      .then(status => {
-        expect(calledParams.to).to.equal("printperson@gmail.com");
-        expect(calledParams.subject).to.equal("NOTE: Upcoming councilmeeting");
-        done();
-      });
-    });
-  });
-  describe("sendProfessorReminder(thesis)", () => {
-    it("should call sendEmail with correct values", (done) => {
-      Reminder.sendProfessorReminder(thesis)
-      .then(status => {
-        expect(calledParams.to).to.equal("ohtugrappa@gmail.com");
-        expect(calledParams.subject).to.equal("REMINDER: Submit your evaluation");
-        done();
-      });
-    });
-  });
-});
