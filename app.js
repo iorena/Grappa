ed06b0aaddefee538cf08cd0647c8650f08e64ca
TeemuKoivisto <tkoivisto456@gmail.com>
2016-04-28 16:37:48 +0300

    Fix it also in app.js...

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const scheduler = require("./services/scheduler");
+const scheduler = require("./services/Scheduler");
 
 const app = express();
 
