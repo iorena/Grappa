cabd2a955eb8bedf018f96d000b13244de4a9661
TeemuKoivisto <tkoivisto456@gmail.com>
2016-10-26 15:07:40 +0300

    Fix print-person unable to download theses

--- controllers/thesis.js
+++ controllers/thesis.js
@@ -157,7 +157,7 @@ module.exports.generateThesesToPdf = (req, res, next) => {
   .then((path) => {
     pathToFile = path;
     if (req.user.role === "print-person") {
-      return Promise.all(thesisIDs.map(thesis_id =>
+      return Promise.all(req.body.map(thesis_id =>
         ThesisProgress.setPrintDone(thesis_id))
       );
     } else {
