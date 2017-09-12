7de871b45b17b6359b3881f4df212dede7907f5b
Jakousa <jakousa@cs.helsinki.fi>
2017-09-12 16:45:27 +0300

    feat: create grader from user

--- controllers/user.js
+++ controllers/user.js
@@ -6,165 +6,168 @@ const PasswordHelper = require("../services/PasswordHelper");
 const SocketIOServer = require("../services/SocketIOServer");
 
 const User = require("../models/User");
+const Grader = require("../models/Grader");
 
 const errors = require("../config/errors");
 
 module.exports.findAll = (req, res, next) => {
   User
-  .findAll()
-  .then(users => {
-    res.status(200).send(users);
-  })
-  .catch(err => next(err));
+    .findAll()
+    .then(users => {
+      res.status(200).send(users);
+    })
+    .catch(err => next(err));
 };
 
 module.exports.updateOne = (req, res, next) => {
   const user = req.body;
 
   Promise.resolve()
-  .then(() => {
-    if (req.user.id.toString() !== req.params.id && req.user.role !== "admin") {
-      throw new errors.ForbiddenError("Missing privileges to edit User.");
-    } else if (req.user.id.toString() === req.params.id && !user.password) {
-      throw new errors.BadRequestError("No password supplied.");
-    } else if (req.user.id.toString() === req.params.id && user.newPassword && user.newPassword.length < 8) {
-      throw new errors.BadRequestError("New password is less than 8 characters.");
-    } else {
-      return User.findOne({ id: req.params.id });
-    }
-  })
-  .then(foundUser => {
-    if (!foundUser) {
-      throw new errors.NotFoundError("No User found.");
-    } else if (req.user.id.toString() === req.params.id && user.password &&
-      !PasswordHelper.comparePassword(user.password, foundUser.passwordHash)) {
-      throw new errors.AuthenticationError("Wrong password.");
-    }
-    let strippedUser = {};
-    if (req.user.id.toString() === req.params.id) {
-      strippedUser = {
-        firstname: user.firstname,
-        lastname: user.lastname,
-        email: user.email,
-      };
-      if (user.newPassword) {
-        strippedUser.passwordHash = PasswordHelper.hashPassword(user.newPassword);
+    .then(() => {
+      if (req.user.id.toString() !== req.params.id && req.user.role !== "admin") {
+        throw new errors.ForbiddenError("Missing privileges to edit User.");
+      } else if (req.user.id.toString() === req.params.id && !user.password) {
+        throw new errors.BadRequestError("No password supplied.");
+      } else if (req.user.id.toString() === req.params.id && user.newPassword && user.newPassword.length < 8) {
+        throw new errors.BadRequestError("New password is less than 8 characters.");
+      } else {
+        return User.findOne({ id: req.params.id });
       }
-    } else {
-      strippedUser = user;
-      if (strippedUser.role === "professor" && strippedUser.StudyFieldId) {
-        return User.findStudyfieldsProfessor(strippedUser.StudyFieldId)
-          .then(prof => {
-            if (prof && prof.id !== strippedUser.id) {
-              throw new errors.BadRequestError("Studyfield already has a professor in charge.");
-            } else {
-              return User.update(strippedUser, { id: req.params.id });
-            }
-          });
+    })
+    .then(foundUser => {
+      if (!foundUser) {
+        throw new errors.NotFoundError("No User found.");
+      } else if (req.user.id.toString() === req.params.id && user.password &&
+        !PasswordHelper.comparePassword(user.password, foundUser.passwordHash)) {
+        throw new errors.AuthenticationError("Wrong password.");
       }
-    }
-    return User.update(strippedUser, { id: req.params.id });
-  })
-  .then(rows => User.findOne({ id: req.params.id }))
-  .then(foundUser => {
-    foundUser.passwordHash = undefined;
-    return SocketIOServer.broadcast(["admin"], [{
-      type: "USER_UPDATE_ONE_SUCCESS",
-      payload: foundUser,
-      notification: `User ${req.user.fullname} updated an User`,
-    }], req.user)
-  })
-  .then(() => {
-    res.sendStatus(200);
-  })
-  .catch(err => next(err));
+      let strippedUser = {};
+      if (req.user.id.toString() === req.params.id) {
+        strippedUser = {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+        };
+        if (user.newPassword) {
+          strippedUser.passwordHash = PasswordHelper.hashPassword(user.newPassword);
+        }
+      } else {
+        strippedUser = user;
+        if (strippedUser.role === "professor" && strippedUser.StudyFieldId) {
+          return User.findStudyfieldsProfessor(strippedUser.StudyFieldId)
+            .then(prof => {
+              if (prof && prof.id !== strippedUser.id) {
+                throw new errors.BadRequestError("Studyfield already has a professor in charge.");
+              } else {
+                return User.update(strippedUser, { id: req.params.id });
+              }
+            });
+        }
+      }
+      return Promise.all([
+        User.update(strippedUser, { id: req.params.id }),
+        Grader.updateGraderFromUser(foundUser, user.graderTitle)
+      ]);
+    })
+    .then(rows => User.findOne({ id: req.params.id }))
+    .then(foundUser => {
+      foundUser.passwordHash = undefined;
+      return SocketIOServer.broadcast(["admin"], [{
+        type: "USER_UPDATE_ONE_SUCCESS",
+        payload: foundUser,
+        notification: `User ${req.user.fullname} updated an User`,
+      }], req.user)
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => next(err));
 };
 
 
 module.exports.saveOne = (req, res, next) => {
-  User
-  .findOne({ email: req.body.email })
-  .then(foundUser => {
-    if (foundUser) {
-      throw new errors.BadRequestError("User already exists with the same email.");
-    } else {
-      req.body.passwordHash = PasswordHelper.hashPassword(req.body.password);
-      return User.saveOne(req.body);
-    }
-  })
-  .then(savedUser => {
-    savedUser.passwordHash = undefined;
-    return SocketIOServer.broadcast(["admin"], [{
-      type: "USER_SAVE_ONE_SUCCESS",
-      payload: savedUser,
-      notification: `User ${savedUser.firstname} ${savedUser.lastname} has registered`,
-    }], savedUser)
-  })
-  .then(() => {
-    res.sendStatus(200);
-  })
-  .catch(err => next(err));
+  User.findOne({ email: req.body.email })
+    .then(foundUser => {
+      if (foundUser) {
+        throw new errors.BadRequestError("User already exists with the same email.");
+      } else {
+        req.body.passwordHash = PasswordHelper.hashPassword(req.body.password);
+        return User.saveOne(req.body);
+      }
+    })
+    .then(savedUser => {
+      savedUser.passwordHash = undefined;
+      return SocketIOServer.broadcast(["admin"], [{
+        type: "USER_SAVE_ONE_SUCCESS",
+        payload: savedUser,
+        notification: `User ${savedUser.firstname} ${savedUser.lastname} has registered`,
+      }], savedUser)
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => next(err));
 };
 
 module.exports.deleteOne = (req, res, next) => {
   User
-  .delete({ id: req.params.id })
-  .then(deletedRows => SocketIOServer.broadcast(["admin"], [{
-    type: "USER_DELETE_ONE_SUCCESS",
-    payload: { id: parseInt(req.params.id) },
-    notification: `Admin ${req.user.fullname} deleted an User`,
-  }], req.user))
-  .then(() => {
-    res.sendStatus(200);
-  })
-  .catch(err => next(err));
+    .delete({ id: req.params.id })
+    .then(deletedRows => SocketIOServer.broadcast(["admin"], [{
+      type: "USER_DELETE_ONE_SUCCESS",
+      payload: { id: parseInt(req.params.id) },
+      notification: `Admin ${req.user.fullname} deleted an User`,
+    }], req.user))
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => next(err));
 };
 
 module.exports.loginUser = (req, res, next) => {
   User
-  .findOne({ email: req.body.email })
-  .then(user => {
-    if (!user) {
-      throw new errors.NotFoundError("No user found with given email.");
-    } else if (!user.isActive) {
-      throw new errors.ForbiddenError("Your account is inactive, please contact admin for activation.");
-    } else if (user.isRetired) {
-      throw new errors.ForbiddenError("Your account has been retired, please contact admin for reactivation.");
-    } else if (!PasswordHelper.comparePassword(req.body.password, user.passwordHash)) {
-      throw new errors.AuthenticationError("Incorrect password.");
-    } else {
-      const payload = TokenGenerator.generateLoginPayload(user);
-      const token = TokenGenerator.generateToken(payload);
-      user.passwordHash = undefined;
-      res.status(200).send({
-        user,
-        token,
-        expires: payload.expires,
-      });
-    }
-  })
-  .catch(err => next(err));
+    .findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        throw new errors.NotFoundError("No user found with given email.");
+      } else if (!user.isActive) {
+        throw new errors.ForbiddenError("Your account is inactive, please contact admin for activation.");
+      } else if (user.isRetired) {
+        throw new errors.ForbiddenError("Your account has been retired, please contact admin for reactivation.");
+      } else if (!PasswordHelper.comparePassword(req.body.password, user.passwordHash)) {
+        throw new errors.AuthenticationError("Incorrect password.");
+      } else {
+        const payload = TokenGenerator.generateLoginPayload(user);
+        const token = TokenGenerator.generateToken(payload);
+        user.passwordHash = undefined;
+        res.status(200).send({
+          user,
+          token,
+          expires: payload.expires,
+        });
+      }
+    })
+    .catch(err => next(err));
 };
 
 module.exports.requestPasswordResetion = (req, res, next) => {
   User
-  .findOne({ email: req.body.email })
-  .then(user => {
-    if (!user) {
-      throw new errors.NotFoundError("No user found with given email.");
-    } else if (!user.isActive) {
-      throw new errors.ForbiddenError("Your account is inactive, please contact admin for activation.");
-    } else if (user.isRetired) {
-      throw new errors.ForbiddenError("Your account has been retired, please contact admin to reactivate.");
-    } else {
-      // find email status where type reset-password, created at today and length of those max 3 ?
-      return EmailReminder.sendResetPasswordMail(user);
-    }
-  })
-  .then(() => {
-    res.sendStatus(200);
-  })
-  .catch(err => next(err));
+    .findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        throw new errors.NotFoundError("No user found with given email.");
+      } else if (!user.isActive) {
+        throw new errors.ForbiddenError("Your account is inactive, please contact admin for activation.");
+      } else if (user.isRetired) {
+        throw new errors.ForbiddenError("Your account has been retired, please contact admin to reactivate.");
+      } else {
+        // find email status where type reset-password, created at today and length of those max 3 ?
+        return EmailReminder.sendResetPasswordMail(user);
+      }
+    })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => next(err));
 }
 
 module.exports.sendNewPassword = (req, res, next) => {
@@ -173,33 +176,33 @@ module.exports.sendNewPassword = (req, res, next) => {
   let generatedPassword;
 
   Promise.resolve(TokenGenerator.verifyToken(req.body.token, { audience: "password" }))
-  .then((decoded) => {
-    if (TokenGenerator.isTokenExpired(decoded)) {
-      throw new errors.BadRequestError("Token has expired.");
-    } else {
-      decodedToken = decoded;
-      return User.findOne({ id: decodedToken.user.id });
-    }
-  })
-  .then(user => {
-    if (!user) {
-      throw new errors.NotFoundError("No user found with given email.");
-    } else if (!user.isActive) {
-      throw new errors.ForbiddenError("Your account is inactive, please contact admin for activation.");
-    } else if (user.isRetired) {
-      throw new errors.ForbiddenError("Your account has been retired, please contact admin to reactivate.");
-    } else {
-      // same as with the above, check for old emails sent and if 3 already (or less?)
-      // then send forbidden error
-      foundUser = user;
-      generatedPassword = PasswordHelper.generatePassword();
-      const passwordHash = PasswordHelper.hashPassword(generatedPassword);
-      return User.update({ passwordHash, }, { id: decodedToken.user.id });
-    }
-  })
-  .then(rows => EmailReminder.sendNewPasswordMail(foundUser, generatedPassword))
-  .then(() => {
-    res.sendStatus(200);
-  })
-  .catch(err => next(err));
+    .then((decoded) => {
+      if (TokenGenerator.isTokenExpired(decoded)) {
+        throw new errors.BadRequestError("Token has expired.");
+      } else {
+        decodedToken = decoded;
+        return User.findOne({ id: decodedToken.user.id });
+      }
+    })
+    .then(user => {
+      if (!user) {
+        throw new errors.NotFoundError("No user found with given email.");
+      } else if (!user.isActive) {
+        throw new errors.ForbiddenError("Your account is inactive, please contact admin for activation.");
+      } else if (user.isRetired) {
+        throw new errors.ForbiddenError("Your account has been retired, please contact admin to reactivate.");
+      } else {
+        // same as with the above, check for old emails sent and if 3 already (or less?)
+        // then send forbidden error
+        foundUser = user;
+        generatedPassword = PasswordHelper.generatePassword();
+        const passwordHash = PasswordHelper.hashPassword(generatedPassword);
+        return User.update({ passwordHash, }, { id: decodedToken.user.id });
+      }
+    })
+    .then(rows => EmailReminder.sendNewPasswordMail(foundUser, generatedPassword))
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(err => next(err));
 };
